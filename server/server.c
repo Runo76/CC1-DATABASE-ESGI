fe71f33c78a1c0616593798cea5a4c7b7938d2d6
Onur ANGELOV <oangelov@myges.fr>
2025-05-03 00:45:16 +0200

    Implement register API for client registration

--- server/server.c
+++ server/server.c
@@ -6,6 +6,44 @@
 #include <arpa/inet.h>
 #include "server.h"
 
+Client clients[MAX_CLIENTS];
+int client_count = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void* handle_client(void* arg) {
+    int client_sock = *(int*)arg;
+    free(arg);
+
+    char buffer[1024];
+    ssize_t len;
+
+    while ((len = recv(client_sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[len] = '\0';
+
+        char *cmd = strtok(buffer, " ");
+        if (cmd && strcmp(cmd, "register") == 0) {
+            char *hostname = strtok(NULL, " ");
+            char *sshkey = strtok(NULL, " ");
+
+            if (hostname && sshkey) {
+                pthread_mutex_lock(&lock);
+                if (client_count < MAX_CLIENTS) {
+                    strncpy(clients[client_count].hostname, hostname, sizeof(clients[client_count].hostname)-1);
+                    strncpy(clients[client_count].sshkey, sshkey, sizeof(clients[client_count].sshkey)-1);
+                    clients[client_count].hostname[sizeof(clients[client_count].hostname)-1] = '\0';
+                    clients[client_count].sshkey[sizeof(clients[client_count].sshkey)-1] = '\0';
+                    client_count++;
+                    printf("New client registered: %s - Key: %s\n", hostname, sshkey);
+                }
+                pthread_mutex_unlock(&lock);
+            }
+        }
+    }
+
+    close(client_sock);
+    return NULL;
+}
+
 int main() {
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
