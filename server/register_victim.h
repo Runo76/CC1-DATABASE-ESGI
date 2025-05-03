6f06375ee57b3fbd02bf0cac63ba3dd0656f0381
Onur ANGELOV <oangelov@myges.fr>
2025-05-03 17:54:36 +0200

    Register_victim.h

---
+++ server/register_victim.h
@@ -0,0 +1,7 @@
+#ifndef REGISTER_VICTIM_H
+#define REGISTER_VICTIM_H
+
+void register_victim(const char* username, const char* password);
+void register_simple(const char* hostname, const char* password);
+
+#endif
